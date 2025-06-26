b9d5baffce3c2ce9e6233c44539f1073e1732b83
mb11jr <bentaleb11jr@gmail.com>
2025-06-26 19:42:17 +0100

    execution: header update

--- execution/exec.h
+++ execution/exec.h
@@ -6,7 +6,7 @@
 /*   By: mbentale <mbentale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 19:02:35 by mbentale          #+#    #+#             */
-/*   Updated: 2025/06/01 07:57:53 by mbentale         ###   ########.fr       */
+/*   Updated: 2025/06/26 19:29:43 by mbentale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # include <stdio.h>
 # include <string.h>
 # include <sys/wait.h>
+# include <sys/stat.h>
 
 # define EXIT_SUCCESS 0
 # define EXIT_FAILURE 1
@@ -36,6 +37,11 @@ typedef struct s_shell
 	t_list	*env;
 }			t_shell;
 
+void		ft_exec(t_list *lst, t_list **env);
+void		exec_builtin(char **args, t_list **env);
+int			is_builtin_cmd(char **args);
+int			ft_execvpe(char *file, char **argv, t_list *env);
+
 // Built-ins
 int			ft_echo(char **args);
 int			ft_pwd(void);
@@ -45,6 +51,12 @@ int			ft_export(char **args, t_list **env);
 int			ft_unset(char **args, t_list **env);
 int			ft_exit(char **args, t_list **env, t_list **lst, char **line);
 // int			ft_exit(char **args, t_list **env);
+
+// Pipe execution
+void	first_child(t_list *lst, int fd[2], pid_t *pid, t_list **env);
+void	second_child(t_list *lst, int fd[2], pid_t *pid, t_list **env);
+void	exec_pipe(t_list *lst, t_list **env);
+void	execute_cmd(char **args);
 
 // env helper functions
 t_env		*env_new(char *key, char *value);
@@ -58,7 +70,5 @@ void		env_del(t_list **env, char *key);
 void		ft_putendl(char *s);
 void		ft_putendl_fd(char *s, int fd);
 void		*ft_malloc(int size);
-
-// Helper functions
 
 #endif // EXEC_H
