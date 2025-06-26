1d353048ef47c07123d4a9d3527443b1be4c93fd
mb11jr <bentaleb11jr@gmail.com>
2025-06-26 19:41:48 +0100

    execution: handling pipes

---
+++ execution/exec_pipe.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_pipe.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbentale <mbentale@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 19:24:24 by mbentale          #+#    #+#             */
+/*   Updated: 2025/06/26 19:31:52 by mbentale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "exec.h"
+
+void	first_child(t_list *lst, int fd[2], pid_t *pid, t_list **env)
+{
+	char	**args;
+
+	args = lst->content;
+	*pid = fork();
+	if (*pid == 0)
+	{
+		close(fd[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+		if (is_builtin_cmd(args))
+		{
+			exec_builtin(args, env);
+			exit(EXIT_SUCCESS);
+		}
+		execvp(args[0], args);
+		perror("larrysh");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	second_child(t_list *lst, int fd[2], pid_t *pid, t_list **env)
+{
+	char	**args;
+
+	args = lst->content;
+	*pid = fork();
+	if (*pid == 0)
+	{
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		if (lst->next)
+		{
+			exec_pipe(lst, env);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			if (is_builtin_cmd(args))
+			{
+				exec_builtin(args, env);
+				exit(EXIT_SUCCESS);
+			}
+			execvp(args[0], args);
+			perror("larrysh");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+void	exec_pipe(t_list *lst, t_list **env)
+{
+	int		pipe_fd[2];
+	pid_t	pids[2];
+
+	pipe(pipe_fd);
+	first_child(lst, pipe_fd, &pids[0], env);
+	second_child(lst->next, pipe_fd, &pids[1], env);
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+	waitpid(pids[0], NULL, 0);
+	waitpid(pids[1], NULL, 0);
+}
+
+void	execute_cmd(char **args)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		execvp(args[0], args);
+		perror("larrysh");
+		exit(EXIT_FAILURE);
+	}
+	waitpid(pid, NULL, 0);
+}
