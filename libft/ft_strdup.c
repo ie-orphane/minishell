4e2dda032f26a89e735176f0cb276cad9bb71f50
mb11jr <bentaleb11jr@gmail.com>
2025-07-15 09:31:06 +0100

    update libft/ft_strdup
    small change, nothing special lol

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mbentale <mbentale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 10:18:35 by mbentale          #+#    #+#             */
-/*   Updated: 2024/10/25 10:55:09 by mbentale         ###   ########.fr       */
+/*   Updated: 2025/07/15 09:11:41 by mbentale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char	*ft_strdup(const char *s1)
 	int		i;
 	char	*copy;
 
-	i = 0;
 	copy = (char *)malloc(ft_strlen(s1) + 1);
-	if (copy == NULL)
+	if (!copy)
 		return (NULL);
+	i = 0;
 	while (s1[i])
 	{
 		copy[i] = s1[i];
